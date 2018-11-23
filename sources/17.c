b26f1baa04aec09a5ad8309f12863d2412706d9d
Sarvendra Kumar Singh <sarvendrakumarsingh.singh4@gmail.com>
2018-11-23 21:12:46 +0530

    Add files via upload

---
+++ sources/17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	int count = 9;
+	while (count >= 9)
+    {
+		for ( int i = 1 ; i < count ; i++ , count-- )
+        {
+			printf("%d",count);
+		}
+	}
+    return 0;
+
+}
+
+
---
+++ sources/18.c
@@ -0,0 +1,12 @@
+
+
+#include <stdio.h>
+int main()
+{
+	int i = 5;
+	while (--i);
+		printf("one");
+	printf("%d", i);
+    return 0;
+
+}
